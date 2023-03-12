c8eece5d1b882346e63bb05a2901d7150fa62440
Michal Urbanek <urbanekmichal.98@gmail.com>
2023-03-12 19:53:33 +0100

    add create and get method for task service

--- src/main/java/com/urbanek/demo/crud/contollers/TaskController.java
+++ src/main/java/com/urbanek/demo/crud/contollers/TaskController.java
@@ -1,9 +1,16 @@
 package com.urbanek.demo.crud.contollers;
 
+import com.urbanek.demo.crud.dto.TaskDto;
+import com.urbanek.demo.crud.dto.TaskDtoCreateResponse;
+import com.urbanek.demo.crud.services.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 @Validated
 @RestController
@@ -11,4 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tasks")
 public class TaskController {
 
+    private final TaskService taskService;
+    @PostMapping("/")
+    public Mono<ResponseEntity<TaskDtoCreateResponse>> addTask(@RequestBody final TaskDto taskDto){
+        return taskService.addTask(taskDto).map(ResponseEntity::ok);
+    }
+
+    @GetMapping("/{taskId}")
+    public Mono<ResponseEntity<TaskDto>> getTask(@PathVariable final UUID taskId){
+        return taskService.getTask(taskId)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
--- src/main/java/com/urbanek/demo/crud/entity/TaskEntity.java
+++ src/main/java/com/urbanek/demo/crud/entity/TaskEntity.java
@@ -1,23 +1,24 @@
 package com.urbanek.demo.crud.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.UUID;
 
 
-@Table(name = "entities")
+@Table(name = "tasks")
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class TaskEntity {
     @Id
-    @Column(name = "task_id")
     private UUID taskId;
-    @Column(name = "name")
+    @Column("name")
     private String name;
-    @Column(name = "description")
+    @Column("description")
     private String description;
 }
