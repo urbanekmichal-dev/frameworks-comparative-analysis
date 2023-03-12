c5befbfc0fe05c9893557eef5114cc2c2454f821
Michal Urbanek <urbanekmichal.98@gmail.com>
2023-03-12 15:49:44 +0100

    add task controller

---
+++ src/main/java/com/urbanek/demo/crud/contollers/TaskController.java
@@ -0,0 +1,14 @@
+package com.urbanek.demo.crud.contollers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tasks")
+public class TaskController {
+
+}
