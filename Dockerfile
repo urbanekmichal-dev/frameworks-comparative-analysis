ba7355a71690313cb9e21798d50ee2689ceb2dd9
Michal Urbanek <urbanekmichal.98@gmail.com>
2023-03-26 01:25:32 +0100

    update spring boot project

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:17
-EXPOSE 8080
-ADD target/demo.jar demo.jar
-ENTRYPOINT ["java","-jar","demo.jar"]
--- src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE SCHEMA IF NOT EXISTS demo;
-
-CREATE TABLE IF NOT EXISTS tasks (
-    task_id uuid default uuid_generate_v4(),
-    name VARCHAR(255),
-      description VARCHAR(255),
-        PRIMARY KEY (task_id)
-                                    );
