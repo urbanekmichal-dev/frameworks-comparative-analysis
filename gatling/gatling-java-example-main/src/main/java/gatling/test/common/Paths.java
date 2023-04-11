bb993ead79b43766e48ad1b08e4b76707ebf8a3d
Michal Urbanek <urbanekmichal.98@gmail.com>
2023-04-11 21:46:22 +0200

    add gatling framework to project

---
+++ gatling/gatling-java-example-main/src/main/java/gatling/test/common/Paths.java
@@ -0,0 +1,5 @@
+package gatling.test.common;
+
+public class Paths {
+    public static final String TASKS = "api/tasks";
+}
---
+++ gatling/gatling-java-example-main/src/main/java/gatling/test/common/ResponseStatus.java
@@ -0,0 +1,6 @@
+package gatling.test.common;
+
+public class ResponseStatus {
+    public static final int OK = 200;
+    public static final int CREATED = 201;
+}
