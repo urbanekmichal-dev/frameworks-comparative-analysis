4a4ff861dece68e771eca6a423817add7959c5c0
Michal Urbanek <urbanekmichal.98@gmail.com>
2023-05-22 11:07:30 +0200

    add NotificationSseSimulation class

---
+++ gatling/gatling-java-example-main/src/main/java/gatling/test/notifications/NotificationSseSimulation.java
@@ -0,0 +1,42 @@
+package gatling.test.notifications;
+
+import gatling.test.common.Constants;
+import gatling.test.common.Paths;
+import gatling.test.common.Properties;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+
+import java.time.Duration;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.sse;
+
+public class NotificationSseSimulation extends Simulation {
+    ScenarioBuilder scn = scenario("GET Notification SSE").forever()
+            .on(exec(sse("Notification SSE").connect(Paths.SSE_NOTIFICATION)
+                    .await(Properties.SSE_WORK_TIME)
+                    .on(sse.checkMessage("Check notification status")
+                            .matching(substring("message"))
+                            .check(regex(".*Current datetime.*"))))
+                    .pause(
+                            Properties.SSE_PAUSE_TIME)
+                    .exec(sse(
+                            "Close").close()));
+
+    {
+        setUp(scn.injectOpen(
+                atOnceUsers(
+                        Properties.getNotificationUsers())))
+                .assertions(
+                        global()
+                                .failedRequests()
+                                .percent()
+                                .lt(Constants.SUCCESS_RATE))
+                .protocols(Constants.getHttpProtocolBuilder())
+                .maxDuration(
+                        Duration.ofMinutes(Properties.getNotificationTime()));
+    }
+
+
+}
+
