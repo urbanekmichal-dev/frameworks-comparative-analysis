51519e7199cb79fbc8dfd3af610bf1b199e7748e
Michal Urbanek <urbanekmichal.98@gmail.com>
2023-05-23 15:22:04 +0200

    update Properties file

--- gatling/gatling-java-example-main/src/main/java/gatling/test/common/Properties.java
+++ gatling/gatling-java-example-main/src/main/java/gatling/test/common/Properties.java
@@ -9,6 +9,8 @@ import static gatling.test.example.simulation.SystemPropertiesUtil.getAsStringOr
 public class Properties {
     private static final String NOTIFICATION_USERS = "10";
     private static final String NOTIFICATION_TIME = "1";
+    private static final String SORTING_USERS = "10";
+    private static final String SORTING_TIME = "1";
     public static final Duration SSE_WORK_TIME = Duration.ofSeconds(10);
     public static final Duration SSE_PAUSE_TIME = Duration.ofSeconds(1);
 
@@ -21,7 +23,12 @@ public class Properties {
     public static int getNotificationTime() {
         return Integer.parseInt(NOTIFICATION_TIME);
     }
-
+    public static int getSortingUsers(){
+        return Integer.parseInt(SORTING_USERS);
+    }
+    public static int getSortingTime(){
+        return Integer.parseInt(SORTING_TIME);
+    }
     public static String getBaseUrl() {
         return BASE_URL;
     }
