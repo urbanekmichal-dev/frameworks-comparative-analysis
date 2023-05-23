c0527b39fbb20f28bacca01f82825e8b3fb1e440
Michal Urbanek <urbanekmichal.98@gmail.com>
2023-05-23 15:32:38 +0200

    update crypto service

--- spring-boot/src/main/java/com/urbanek/demo/crypto/controller/CryptoController.java
+++ spring-boot/src/main/java/com/urbanek/demo/crypto/controller/CryptoController.java
@@ -1,6 +1,8 @@
 package com.urbanek.demo.crypto.controller;
 
 import com.urbanek.demo.crypto.dto.CryptoDtoRequest;
+import com.urbanek.demo.crypto.dto.CryptoDtoResponse;
+import com.urbanek.demo.crypto.dto.EncryptDtoResponse;
 import com.urbanek.demo.crypto.service.CryptoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
@@ -18,17 +20,16 @@ public class CryptoController {
 
     @PostMapping(value = "/encryptText")
     @ResponseBody
-    public Mono<String> encryptText(@RequestBody final CryptoDtoRequest cryptoDtoRequest) {
-        Mono<byte[]> text = cryptoService.encryptText(cryptoDtoRequest.getData());
-
-        return text.flatMap(cryptoService::saveEncryptedText)
-                .thenReturn("Text was encrypted and saved on the server");
+    public Mono<EncryptDtoResponse> encryptText(@RequestBody final CryptoDtoRequest cryptoDtoRequest) {
+        return cryptoService.encryptText(cryptoDtoRequest.getData());
     }
 
-    @GetMapping(value = "/decryptFile/{fileName}", produces = MediaType.TEXT_PLAIN_VALUE)
+    @GetMapping(value = "/decryptFile/{fileName}")
     @ResponseBody
-    public Mono<String> decryptFile(@PathVariable String fileName) {
+    public Mono<CryptoDtoResponse> decryptFile(@PathVariable String fileName) {
         return cryptoService.decryptFile(fileName);
     }
+
+
 
 }
